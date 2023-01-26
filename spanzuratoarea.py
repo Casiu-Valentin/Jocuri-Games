5d5af79bf114b10a912575d5c8a447e1515a5242
George Valentin Casiu <casrom@gmail.com>
2023-01-26 21:40:53 +0200

    Add files via upload

---
+++ spanzuratoarea.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.goto(0,200)
+turtle.write("SPANZURATOAREA",align="center",font=('Arial', 18, 'normal'))
+#a=turtle.textinput("SPANZURATOAREA", "KJADFG;GH;JK")
+#print(a)
+input()
